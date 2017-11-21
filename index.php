fa3f3a79bd64340dc22461f88ae3549d70e89e76
Supod Mepoo <supodmepoo@gmail.com>
2017-11-21 21:22:05 +0700

    commit

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+
+require_once __DIR__.'/vendor/autoload.php';
+
+$token_access 	= 'slhaMqaHkp1bscJ8uOp059U33TW/1Oh09GOG8M3QJdL9pGu29+ibRJYHW5fsi+l8zft0WjPmVRrZa/lnZyR/k3iU+mOqOvwNirBFEptNQHaf4br0APfSxbqEknnZ34pVe4PGE4S9Qwuid1ImsPJ/1wdB04t89/1O/w1cDnyilFU=';
+$httpClient 	= new \LINE\LINEBot\HTTPClient\CurlHTTPClient($token_access);
+$bot 			= new \LINE\LINEBot($httpClient, ['channelSecret' => '798a6fe2afc3c6a5a763f8cd5f3d368a']);
+
+
+$response = $bot->replyText('nHuyWiB7yP5Zw52FIkcQobQuGDXCTA', 'hello!');
