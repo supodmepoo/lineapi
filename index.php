8b86e2606df4f5b88207e0719c4f1058750362a5
supodmepoo <supodmepoo@gmail.com>
2017-11-26 11:19:46 +0700

    Update index.php
    update

--- index.php
+++ index.php
@@ -1,47 +1,26 @@
 <?php
-$access_token = 'slhaMqaHkp1bscJ8uOp059U33TW/1Oh09GOG8M3QJdL9pGu29+ibRJYHW5fsi+l8zft0WjPmVRrZa/lnZyR/k3iU+mOqOvwNirBFEptNQHaf4br0APfSxbqEknnZ34pVe4PGE4S9Qwuid1ImsPJ/1wdB04t89/1O/w1cDnyilFU=';
+require_once __DIR__.'/vendor/autoload.php';
 
 // Get POST body content
 $content = file_get_contents('php://input');
 // Parse JSON
 $events = json_decode($content, true);
+
+$token_access 	= 'slhaMqaHkp1bscJ8uOp059U33TW/1Oh09GOG8M3QJdL9pGu29+ibRJYHW5fsi+l8zft0WjPmVRrZa/lnZyR/k3iU+mOqOvwNirBFEptNQHaf4br0APfSxbqEknnZ34pVe4PGE4S9Qwuid1ImsPJ/1wdB04t89/1O/w1cDnyilFU=';
+$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($token_access);
+$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => '798a6fe2afc3c6a5a763f8cd5f3d368a']);
+
+$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('hello');
+
 // Validate parsed JSON data
 if (!is_null($events['events'])) {
 	// Loop through each event
 	foreach ($events['events'] as $event) {
 		// Reply only when message sent is in 'text' format
 		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
-			// Get text sent
-			$text = $event['message']['text'];
-			// Get replyToken
 			$replyToken = $event['replyToken'];
-
-			// Build message to reply back
-			$messages = [
-				'type' => 'text',
-				'text' => $text
-			];
-
-			// Make a POST Request to Messaging API to reply to sender
-			$url = 'https://api.line.me/v2/bot/message/reply';
-			$data = [
-				'replyToken' => $replyToken,
-				'messages' => [$messages],
-			];
-			$post = json_encode($data);
-			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
-
-			$ch = curl_init($url);
-			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
-			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-			$result = curl_exec($ch);
-			curl_close($ch);
-
-			echo $result . "\r\n";
+			$response = $bot->replyMessage($replyToken, $textMessageBuilder);
+			echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
 		}
 	}
 }
-echo "OK";
